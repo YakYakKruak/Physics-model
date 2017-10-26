88b9d96caf66fbf08a14e6c28903cb659627a16d
semitro <semitro_8@mail.ru>
2017-10-27 01:10:20 +0300

    first commit @3

---
+++ src/vt/smt/Render/Arrow.java
@@ -0,0 +1,12 @@
+package vt.smt.Render;
+
+import javafx.scene.layout.Pane;
+
+/**
+ * Created by semitro on 27.10.17.
+ */
+public class Arrow extends Pane {
+    public Arrow(){
+        super();
+    }
+}
