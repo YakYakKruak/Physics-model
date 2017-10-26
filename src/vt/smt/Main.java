043e55fd0e76f299434654cc4a07030fe78ddf70
semitro <semitro_8@mail.ru>
2017-10-27 01:37:57 +0300

    vectors field init

--- src/vt/smt/Main.java
+++ src/vt/smt/Main.java
@@ -7,11 +7,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Hello World");
+
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
