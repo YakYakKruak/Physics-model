e01007b904326b7f58e824cbc96d74d1228abc92
YakYakKruak <golovanev98@mail.ru>
2017-11-16 23:07:25 +0300

    it's steel beautiful!

--- src/vt/smt/Controller.java
+++ src/vt/smt/Controller.java
@@ -1,7 +1,6 @@
 package vt.smt;
 
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
 import javafx.scene.control.ContextMenu;
@@ -21,44 +20,28 @@ public class Controller {
     @FXML private VectorField field;
     @FXML private vt.smt.Render.Charge prototype_positive; // Заряды-меню внизу
     @FXML private vt.smt.Render.Charge prototype_negative;
-    private vt.smt.Physics.Charge      charge_positive_prototype; // Физический
-    private vt.smt.Physics.Charge      charge_negaitive_prototype;
-
 
     private ContextMenu contextMenu;
     private List<vt.smt.Render.Charge> charges = new LinkedList<>();
     private VectorFieldCalculator calculator = new VectorFieldCalculatorImpl();
     private MenuItem  add = new MenuItem("Добавить заряд");
-
-
-
     public void initialize(){
-        calculator.setКондюк(new Кондюк(new Point2D(800,200),300,30,400,10E-9));
+        calculator.setКондюк(new Кондюк(new Point2D(200,400),300,30,600,10E-4));
         initContextMenu();
-        init_prototypes();
+       init_prototypes();
+       field.setFieldByAngle(calculator.getVectorAngleInPoint());
 
     }
     private void init_prototypes(){
         Platform.runLater(()->{
-            charge_negaitive_prototype =  new vt.smt.Physics.Charge(-10,new Point2D(0,0));
-            charge_positive_prototype  =  new vt.smt.Physics.Charge(10,new Point2D(0,0));
-            prototype_negative.setCharge( charge_negaitive_prototype);
-            prototype_positive.setCharge( charge_positive_prototype);
-            inital_prototype_1_pos = new Point2D(prototype_negative.getTranslateX(), prototype_negative.getTranslateY());
-            inital_prototype_2_pos = new Point2D(prototype_positive.getTranslateX(), prototype_positive.getTranslateY());
-
-            EventHandler<MouseEvent> on_drag_done = m-> {
-              if(field.getHeight() - m.getSceneY() > 0 ) {
-
-                  double chrg = ((vt.smt.Render.Charge)m.getSource()).getCharge().getCharge();
-                  add_new_charge(new vt.smt.Physics.Charge(chrg, new Point2D(m.getSceneX(), m.getSceneY())));
-
-
-              }
+            prototype_negative.setCharge( new vt.smt.Physics.Charge(-10,new Point2D(0,0)));
+            prototype_positive.setCharge( new vt.smt.Physics.Charge(10,new Point2D(0,0)) );
+            Runnable on_drag_done = ()->{
+                System.out.println("back");
             };
-            prototype_positive.setOnMouseReleased(on_drag_done);
-            prototype_negative.setOnMouseReleased(on_drag_done);
-//
+            prototype_positive.setOnDragDone(e->on_drag_done.run());
+            prototype_positive.setWhileDragging(()->{
+            });
         });
 
     }
@@ -67,40 +50,24 @@ public class Controller {
 
     private Point2D lastClick; // to set the charge after the click to a proper position
 
-    private void add_new_charge(vt.smt.Render.Charge render_charge){
-        vt.smt.Render.Charge newCharge = new vt.smt.Render.Charge(render_charge.getCharge());
-        charges.add(newCharge);
-        calculator.addCharge(newCharge.getCharge());
-        field.getChildren().add(render_charge);
-        newCharge.setWhileDragging(()->Platform.runLater(()->field.setFieldByAngle(calculator.getVectorAngleInPoint())));
-
-        Platform.runLater(()->field.setFieldByAngle(calculator.getVectorAngleInPoint()));
-    }
-    private void add_new_charge(vt.smt.Physics.Charge phys_charge){
-        vt.smt.Render.Charge newCharge = new vt.smt.Render.Charge(phys_charge);
-        add_new_charge(newCharge);
-    }
     private void initContextMenu(){
         class Костыль{
             public int x;
         }
         Костыль t1 = new Костыль();
-        
+
         add.setOnAction(e->{
             Platform.runLater(()->{
-                add_new_charge(new Charge(1E+8,lastClick));
-//                vt.smt.Physics.Charge new_phys_charge;
-//                new_phys_charge = new Charge(1E+8,lastClick);
-//
-//                if(t1.x++ % 2 == 0)
-//                    new_phys_charge.setCharge(new_phys_charge.getCharge()*-1);
-//
-//                vt.smt.Render.Charge newCharge = new vt.smt.Render.Charge(new_phys_charge);
-//                newCharge.setWhileDragging(()->Platform.runLater(()->field.setFieldByAngle(calculator.getVectorAngleInPoint())));
-//                charges.add(newCharge);
-//                calculator.addCharge(new_phys_charge);
-//                field.getChildren().add(charges.get(charges.size()-1));
-//                Platform.runLater(()->{field.setFieldByAngle(calculator.getVectorAngleInPoint());});
+                vt.smt.Physics.Charge new_phys_charge;
+                new_phys_charge = new Charge(0,lastClick);
+                if(t1.x++ % 2 == 0)
+                    new_phys_charge.setCharge(new_phys_charge.getCharge()*-1);
+                vt.smt.Render.Charge newCharge = new vt.smt.Render.Charge(new_phys_charge);
+                newCharge.setWhileDragging(()->Platform.runLater(()->field.setFieldByAngle(calculator.getVectorAngleInPoint())));
+                charges.add(newCharge);
+                calculator.addCharge(new_phys_charge);
+                field.getChildren().add(charges.get(charges.size()-1));
+                Platform.runLater(()->{field.setFieldByAngle(calculator.getVectorAngleInPoint());});
             });
 
         });
