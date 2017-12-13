69bd7168366189cdb92ef3e58efda6f8f0c6925a
semitro <semitro_8@mail.ru>
2017-12-13 22:09:59 +0300

    Добавлены иконочки

--- src/vt/smt/Main.java
+++ src/vt/smt/Main.java
@@ -3,6 +3,7 @@ package vt.smt;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -12,9 +13,13 @@ public class Main extends Application {
 
         Scene scene = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Статическое электрическое поле");
+        primaryStage.setIconified(true);
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/icons/icon64.png")));
+
         primaryStage.setFullScreen(true);
         primaryStage.setScene(scene);
         scene.getStylesheets().add(getClass().getResource("/styles/violet.css").toExternalForm());
+
         primaryStage.show();
     }
 
